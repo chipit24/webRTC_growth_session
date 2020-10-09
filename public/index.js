1517c3f32c45e666eb860d1d897dd9017edfba42
Robert Komaromi <robertkomaromi@gmail.com>
2020-10-08 20:59:33 -0400

    Allow stopping of video

--- public/index.js
+++ public/index.js
@@ -42,20 +42,18 @@ webSocketConnection.addEventListener('message', async messageEvent => {
   }
 });
 
-async function streamLocalVideo() {
-  let stream = null;
+let localStream = null;
+let videoSender = null;
+let audioSender = null;
 
+async function streamLocalVideo() {
   try {
-    console.log('getting stream');
-    stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: false,
-    });
-    console.log('got stream');
-    stream
-      .getTracks()
-      .forEach(track => peerConnection.addTrack(track, stream));
-    document.getElementById('local-video').srcObject = stream;
+    const mediaStreamConstraints = { video: true, audio: true };
+    localStream = await navigator.mediaDevices.getUserMedia(mediaStreamConstraints);
+    document.getElementById('local-video').srcObject = localStream;
+
+    videoSender = peerConnection.addTrack(localStream.getVideoTracks()[0], localStream);
+    audioSender = peerConnection.addTrack(localStream.getAudioTracks()[0], localStream);
 
     const offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
@@ -68,7 +66,6 @@ async function streamLocalVideo() {
 }
 
 streamLocalVideo();
-console.log('streamLocalVideo');
 
 // peerConnection.ontrack = function onTrack({ streams: [stream] }) {
 //   const remoteVideo = document.getElementById('remote-video');
@@ -76,12 +73,9 @@ console.log('streamLocalVideo');
 // };
 
 // const remoteStream = new MediaStream();
-// const remoteVideo = document.getElementById('remote-video');
-// remoteVideo.srcObject = remoteStream;
-
+const remoteVideo = document.getElementById('remote-video');
 peerConnection.addEventListener('track', event => {
   // remoteStream.addTrack(event.track, remoteStream);
-  const remoteVideo = document.getElementById('remote-video');
   remoteVideo.srcObject = event.streams[0];
 
   console.log('peerConnection track event', event);
@@ -91,11 +85,40 @@ peerConnection.addEventListener('icecandidate', event => {
   console.log('icecandidate', event.candidate);
   if (event.candidate) {
     webSocketConnection.send(JSON.stringify({ iceCandidate: event.candidate }));
+    console.log('sending iceCandidate');
   }
 });
 
 peerConnection.addEventListener('connectionstatechange', () => {
+  console.log('peer connection changed');
   if (peerConnection.connectionState === 'connected') {
     console.log('peer connected!');
+  }
+});
+
+
+document.getElementById('stop-video').addEventListener('click', async () => {
+  // console.log('videoSender', videoSender);
+  try {
+    // peerConnection.removeTrack(videoSender);
+    // localStream.getVideoTracks()[0].enabled = false;
+
+    console.log('Before stop getSenders', peerConnection.getSenders());
+    // console.log(await peerConnection.getStats(localStream.getVideoTracks()[0]));
+    localStream.getVideoTracks()[0].stop();
+    peerConnection.removeTrack(videoSender);
+    // await videoSender.replaceTrack(null);
+    setTimeout(() => {
+      console.log('Stopped video track');
+      console.log('Stopped video track getSenders', peerConnection.getSenders());
+      // console.log(await peerConnection.getStats(localStream.getVideoTracks()[0]));
+    }, 1000);
+
+    // peerConnection.removeTrack(audioSender);
+    // localStream.getAudioTracks()[0].enabled = false;
+    localStream.getAudioTracks()[0].stop();
+    // peerConnection.close();
+  } catch (error) {
+    console.log('Error stopping your video: ', error);
   }
 });
